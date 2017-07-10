28b651eac53836d615c75846f0db66256f534f54
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-09 20:56:23 -0700

    Created 16x16 grid of squares

--- application.js
+++ application.js
@@ -1,28 +1,8 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-for (var i = 0; i >=16; i++){
-//iterate through loop 256 times, creating a div each time to construct a 16x16 grid
-
+for (var i = 0; i < 16 ; i++) {
+	for(var x = 0; x < 16; x++){
+	$('#pad-container').append('<div class="square"></div>');
+	}
+	var z=16;
+	$('.square').css({'height' : 'calc(500px / '+ z, 'width' : 'calc(500px / '+z});
 
 }
