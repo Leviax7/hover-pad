271c6fb3329f298939e53d55c986fc747653b53a
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-09 23:39:52 -0700

    Change pad size functionality working

--- application.js
+++ application.js
@@ -1,14 +1,36 @@
-for (var i = 0; i < 16 ; i++) {
-	for(var x = 0; x < 16; x++){
-	$('#pad-container').append('<div class="square"></div>');
-	}
-	$('.square').css({'height' : 'calc(500px / 16', 'width' : 'calc(500px / 16'});
+var padsize = 16;
+createPad(padsize);
+
+function draw(){
+	$('.square').on('mouseenter', function(){
+		$(this).css('background-color', 'black');
+	});
 }
 
-$('.square').on('mouseenter', function(){
-	$(this).css('background-color', 'black');
-});
+function createPad(n){
+	for (var i = 0; i < n ; i++) {
+		for(var x = 0; x < n; x++){
+			$('#pad-container').append('<div class="square"></div>');
+		}
+		$('.square').css({'height' : 'calc(500px / ' + n, 'width' : 'calc(500px / ' + n});
+	}
+}
+draw();
 
+$('.pad-size').on('click', function(){
+	var padsize = prompt("Please enter a new pad size between 2 and 64");
+	$('.square').remove();
+	createPad(padsize);
+	draw();
+
+});
 $('.clear').on('click', function(){
 	$('.square').css('background-color', 'transparent');
+});
+
+$('.button').hover(function(){
+$(this).toggleClass('highlighted');
+});
+$('.button').mouseup(function(){ 
+	this.blur();
 });
