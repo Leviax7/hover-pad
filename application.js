bd61464b2f40e9ce43ec4df67082ddc4fbe70705
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-09 21:03:49 -0700

    Squares color on mouse enter

--- application.js
+++ application.js
@@ -2,7 +2,9 @@ for (var i = 0; i < 16 ; i++) {
 	for(var x = 0; x < 16; x++){
 	$('#pad-container').append('<div class="square"></div>');
 	}
-	var z=16;
-	$('.square').css({'height' : 'calc(500px / '+ z, 'width' : 'calc(500px / '+z});
-
+	$('.square').css({'height' : 'calc(500px / 16', 'width' : 'calc(500px / 16'});
 }
+
+$('.square').on('mouseenter', function(){
+	$(this).css('background-color', 'black');
+});
